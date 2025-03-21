520b16e37277a3a8c80b4d32a3cfe1a2d4a9832a
CharieDelRosario <chariedlrs12@gmail.com>
2025-03-22 00:04:45 +0800

    Create ifelse.js

---
+++ ifelse.js
@@ -0,0 +1,14 @@
+let charie = "Pretty"
+
+let charieAppearance;
+
+if(charie == "Pretty")
+{
+  charieAppearance="Charie Pretty"
+}
+else
+{
+  charieAppearance="Charie Pogi"
+}
+
+console.log(charieAppearance);
