b9056316081539f743ed862afdc92682470d4e54
CharieDelRosario <chariedlrs12@gmail.com>
2025-03-22 00:32:38 +0800

    ComparisonOperators.js

---
+++ ComparisonOperators.js
@@ -0,0 +1,29 @@
+console.log("Comparison Operators");
+
+
+console.log("Ternary Operator");
+let charie = "Pretty"
+
+let charieAppearance = (charie == 'Pretty')? "Charie Pretty" : "Charie Pogi";
+
+console.log(charieAppearance);
+
+
+let x = 10;
+let y = 19;
+
+let result = x == y;
+console.log("10 is equal to 19 is",result);
+
+result = x < y;
+console.log("10 is less than to 19 is",result);
+
+result = x > y;
+console.log("10 is greater than to 19 is",result);
+
+result = x != y;
+console.log("10 is not equal to 19 is",result);
+
+result = x === y;
+console.log("10 is equal value or equal type to 19 is", result);
+
