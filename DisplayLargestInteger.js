906a797fd1c23e8503c09c5f865e7b4d0b08409c
CharieDelRosario <chariedlrs12@gmail.com>
2025-03-24 21:26:52 +0800

    Create DisplayLargestInteger.js

---
+++ DisplayLargestInteger.js
@@ -0,0 +1,14 @@
+console.log("This is to display the largest  integer among 50 and 100. \n ");
+
+let num1 = 50;
+let num2 = 100;
+let value = num1 && num2; 
+
+if (num1 > num2) 
+{
+  console. log(value, " is bigger.");
+} 
+else (num2>num1)
+{
+  console.log(value, " is bigger.");
+}
