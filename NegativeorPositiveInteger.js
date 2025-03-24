2ce807c3034c328a8d46926ddf72bc03efe1ecf0
CharieDelRosario <chariedlrs12@gmail.com>
2025-03-24 21:28:49 +0800

    Create NegativeorPositiveInteger.js

---
+++ NegativeorPositiveInteger.js
@@ -0,0 +1,10 @@
+console.log("This is to check if the given number is a negative or a positive number. \n ");
+
+
+let number = -39;
+
+if (number < 0) {
+  console. log(number, " is negative.");
+} else {
+  console.log(number, " is positive ");
+}
