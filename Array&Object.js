986bb1abf2732f95d420a2ac37cbc519a77770a3
CharieDelRosario <chariedlrs12@gmail.com>
2025-03-26 22:07:30 +0800

    Create Array&Object.js
    Displaying User Information Using Array and Object

---
+++ Array&Object.js
@@ -0,0 +1,19 @@
+
+//ARRAY
+let userInfo = [' Charie Del Rosario',17, 'years old', ' Balingit']
+
+console.log(userInfo);
+
+
+//OBJECT
+let userInfo= {
+  Name: 'Charie Del Rosario' ,
+  Age: 17 ,
+  Grade: 12, 
+  Height: 5.1 ,
+  Weight: 49 ,
+  Address: 'Balingit' , 
+  
+
+}
+console.log(userInfo);
