943fb829c622e880a4a893a7539549340393650e
CharieDelRosario <chariedlrs12@gmail.com>
2025-03-27 23:22:24 +0800

    Create CircleArea.js
    finding the area of a circle

---
+++ CircleArea.js
@@ -0,0 +1,9 @@
+console.log("This program is to Calculate the area of a Circle");
+
+const PI = 3.14159;
+
+let radius = 5;
+
+let circleArea = PI * radius **2;
+
+console.log('The area of the curcle is: ', circleArea);
