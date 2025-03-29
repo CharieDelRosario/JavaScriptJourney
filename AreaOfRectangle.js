b397a64736b13704fa46ba9bd0fdd38306695def
CharieDelRosario <chariedlrs12@gmail.com>
2025-03-29 21:50:30 +0800

    Create AreaOfRectangle.js
    Finding the area of Rectangle

---
+++ AreaOfRectangle.js
@@ -0,0 +1,18 @@
+//Formula: area=length*width
+let length = 3;
+let width = 5;
+let area= length * width;
+console.log("The area of a Rectangleis: ", area);
+
+
+
+function areaofRectangle(length,width) 
+  {
+    if(length >=0 && width >= 0)
+  {
+    let rectangleArea = length * width;
+    console.log("The area of Rectangle is: ",rectangleArea);
+  }
+  }
+  
+  areaofRectangle(7,5)
