3565b8766ed56959e750b967bb83a17449588005
CharieDelRosario <chariedlrs12@gmail.com>
2025-04-05 21:41:51 +0800

    Create FarmProblem.js
    In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species: chicken, cow, and pig.

---
+++ FarmProblem.js
@@ -0,0 +1,10 @@
+console.log(" In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species: chicken,cow,pig");
+
+function animallegs(chickenleg,cowleg,pigleg)
+{
+  let chicken=10;
+  let cow= 5;
+  let pig=3;
+  console.log(`10 Chicken= ${chicken*chickenleg} legs \n 5 Cow = ${cow*cowleg} legs \n 3 Pig = ${pig*pigleg} legs`);
+}
+animallegs(2,4,4)
