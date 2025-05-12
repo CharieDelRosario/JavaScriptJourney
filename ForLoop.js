dc8fe69604e9fdfdb6b0d03b96037d450cfff2ef
CharieDelRosario <chariedlrs12@gmail.com>
2025-05-12 13:07:00 +0800

    Create ForLoop.js
    1st Forloop Done

---
+++ ForLoop.js
@@ -0,0 +1,9 @@
+console.log("VOTE WISELY!!");
+const voters=["Lorenza Del Rosario,","Nestor Del Rosario","Kullanay Del Rosario",
+"Kulawlaw Del Rosario","Talok Del Rosario","Lampaso Del Rosario","Akki Del Rosario","Pintas Del Rosario","Bilinan Del Rosario","Alili Del Rosario"];
+
+
+for (i=0;i<10;i++)
+{
+  console.log(i+1,voters[i]);
+}
