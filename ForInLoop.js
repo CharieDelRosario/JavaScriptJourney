0df86b4dec3b6652d56ade3a8364d1b0ed39ee23
CharieDelRosario <chariedlrs12@gmail.com>
2025-05-15 23:50:18 +0800

    Create ForInLoop.js

---
+++ ForInLoop.js
@@ -0,0 +1,12 @@
+console.log("For in Loop\n");
+const rainbow=['Red','Orange','Yellow','Green','Blue','Indigo','Violet']
+
+for(color in rainbow)
+{
+  if(color==3)
+  {
+   //break;
+  //continue;
+}
+console.log(rainbow[color]);
+}
