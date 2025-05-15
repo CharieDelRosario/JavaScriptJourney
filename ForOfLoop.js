5b4dc6d7ab4a9f0820c13a497cc9ac4df6623d7d
CharieDelRosario <chariedlrs12@gmail.com>
2025-05-15 23:53:10 +0800

    Create ForOfLoop.js

---
+++ ForOfLoop.js
@@ -0,0 +1,12 @@
+console.log("For of Loop\n");
+const rainbow=['Red','Orange','Yellow','Green','Blue','Indigo','Violet']
+
+for(color of rainbow)
+{
+  if(color=='Orange')
+  {
+   //break;
+  //continue;
+}
+console.log(color);
+}
