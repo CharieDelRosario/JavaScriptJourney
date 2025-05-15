cd6f31e12276d0fa111c5c615c545a2eaccb1b9e
CharieDelRosario <chariedlrs12@gmail.com>
2025-05-15 23:36:45 +0800

    Create ForLoop.js

---
+++ ForLoop.js
@@ -0,0 +1,12 @@
+console.log("For Loop\n");
+const rainbow=['Red','Orange','Yellow','Green','Blue','Indigo','Violet']
+
+for(r=0;r<=6;r++)
+{
+  if(rainbow[r]=='Orange')
+  {
+   //break;
+  //continue;
+}
+console.log(rainbow[r]);
+}
